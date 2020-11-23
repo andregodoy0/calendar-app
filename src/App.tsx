aeaaa3394c53733c1ce3aa9ee37b532688f42337
Andre Godoy <andregodoy0@gmail.com>
2020-11-22 22:47:18 -0300

    Fix calendar layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Calendar from './Calendar';
 
 function App() {
     return (
-      <div className="App">
+      <div className="app">
           <h1>Welcome to Calendar App!</h1>
           <h3>Select one day of the month and add/edit your tasks!</h3>
           <div>Remenber that today is {moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</div>
--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,63 +1,67 @@
 import React from 'react'
 import moment from 'moment'
+import classnames from 'classnames'
 
 import './Calendar.scss';
-import { Grid, Card, CardHeader, CardContent } from '@material-ui/core';
+import { Grid, Card, CardHeader, CardContent, IconButton } from '@material-ui/core'
+import AddCircleRoundedIcon from '@material-ui/icons/AddCircleRounded'
 
 function Calendar() {
-    const now = moment()
-    const numberOfDaysLastMonth = now.daysInMonth()
-    const dayOfWeekFirstDayOfMonth = now.startOf('month').day()
-    // const dayOfWeek = now.day()
-    // const dayOfMonth = now.date()
-
-    const renderFirstMonth = () => {
-        const cells = []
-        for (let i=0; i<numberOfDaysLastMonth; i++) {
-            cells.push(
-                <Card variant="outlined" square className='cell' key={i}>
-                    <CardHeader title={i+1} />
-                    <CardContent>
-                        Add your tasks!
-                    </CardContent>
-                </Card>
-            )
-        }
+    
+    const renderCalendarHeader = () => {
+        const columns = ['Sunday', 'Monday', 'Tuesday', 'Wdnsday', 'Thursday', 'Friday', 'Saturday']
+        return columns.map(dayOfWeek => (
+            <Card variant='outlined' className='header cell' key={ dayOfWeek }>
+                <CardHeader title={ dayOfWeek } />
+            </Card>
+        ))
+    }
+    const createCalendarDay = (day: number, shouldGrayout: boolean) => {
         return (
-            cells
+            <Card variant="outlined" square className={classnames('calendar-day', {'grayed-out': shouldGrayout})} key={ day }>
+                <CardHeader title={ day } />
+                <CardContent>
+                </CardContent>
+                    <IconButton aria-label="Create new task">
+                        <AddCircleRoundedIcon />
+                    </IconButton>
+            </Card>
         )
     }
-    const getColumns = () => {
-        const columns = ['Sunday', 'Monday', 'Tuesday', 'wWdnsday', 'Thursday', 'Friday', 'Saturday']
-        return columns.map(day => ({
-            field: day,
-            headerName: day,
-        }))
-    }
-    const getRows = () => {
+    const renderCurrentMonth = () => {
+        const numberOfDaysPreviousMonth = moment().subtract(1, 'month').daysInMonth()
+        const numberOfDaysCurrentMonth = moment().daysInMonth()
         const cells = []
-        for (let i=0; i<numberOfDaysLastMonth; i++) {
-            cells.push(
-                <Grid xs className='cell' key={i}>
-                    {i+1}
-                </Grid>
-            )
+        for (let i = 0; i < numberOfDaysCurrentMonth; i++) {
+            cells.push(createCalendarDay(i + 1, true))
         }
-        return 
+        // Append calendar days for previous month at the begining of the array
+        const dayOfWeekFirstDayOfMonth = moment().startOf('month').day()
+        if (dayOfWeekFirstDayOfMonth) {
+            for (let i = dayOfWeekFirstDayOfMonth; i >= 0; i--) {
+                cells.unshift(createCalendarDay(numberOfDaysPreviousMonth - i, false))
+            }
+        }
+        const dayOfWeekLastDayOfMonth = moment().endOf('month').day()
+        // Append calendar days for noxt month at the end of the array
+        if (dayOfWeekLastDayOfMonth !== 6) {
+            for (let i = 0; i < (6 - dayOfWeekLastDayOfMonth); i++) {
+                cells.push(createCalendarDay(i + 1, false))
+            }
+        }
+        return cells
     }
+
     return (
         <div>
-            <div>Hello from calendar!</div>
+            <div>We are in {moment().format('MMMM')}</div>
             <div className='calendar'>
-                <Grid
-                    container
-                >
-
-                    {renderFirstMonth()}
+                <Grid container>
+                    { renderCalendarHeader() }
+                    { renderCurrentMonth() }
                 </Grid>
             </div>
         </div>
-
     )
 }
 
