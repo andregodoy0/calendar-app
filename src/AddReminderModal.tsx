8d103f4093ed841c0b9fa95c00cddf825769c2ab
Andre Godoy <andregodoy0@gmail.com>
2020-11-26 00:39:55 -0300

    Add functional color picker

--- src/AddReminderModal.tsx
+++ src/AddReminderModal.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react'
 import { Moment } from 'moment'
-import { Paper, TextField } from '@material-ui/core';
-import ColorPicker from 'material-ui-color-picker'
+import { Button, Paper, TextField } from '@material-ui/core'
+import { GithubPicker } from 'react-color'
+
+import './AddReminderModal.scss'
 
 interface AddReminderModalProps {
     dayOfMonth: Moment
 }
+interface Reminder {
+    reminder: string
+    time: string
+    color: string 
+    city: string
+}
+
 const AddReminderModal: React.FunctionComponent<AddReminderModalProps> = ({dayOfMonth}) => {
+    const [reminderData, setReminderData] = useState({
+        color: '#008B02',
+        time: dayOfMonth.format("HH:MM")
+    } as Reminder)
+    const updateReminder = (updatedData: Partial<Reminder>) => {
+        setReminderData({
+            ...reminderData,
+            ...updatedData,
+        })
+    }
+    const submitForm = () => {
+        console.log(reminderData)
+    }
     return (
         <Paper>
             <h2>Create new reminder:</h2>
@@ -14,17 +36,17 @@ const AddReminderModal: React.FunctionComponent<AddReminderModalProps> = ({dayOf
                 <TextField
                     fullWidth
                     type='text'
+                    value={reminderData.reminder}
                     label='Remind me to'
+                    onChange={event => updateReminder({reminder: event.target.value})}
                 />
-                <label>on {dayOfMonth.format('MMM DD')} at</label>
+                <label></label>
                 <TextField
                     id='time'
-                    label='time'
+                    label={`on ${dayOfMonth.format('MMM DD')} at`}
                     type="time"
-                    defaultValue={dayOfMonth.format("HH:MM")}
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
+                    value={reminderData.time}
+                    onChange={event => updateReminder({time: event.target.value})}
                     inputProps={{
                         step: 1500, // 15 min
                     }}
@@ -33,14 +55,21 @@ const AddReminderModal: React.FunctionComponent<AddReminderModalProps> = ({dayOf
                     fullWidth
                     type='text'
                     label='City'
+                    value={reminderData.city}
+                    onChange={event => updateReminder({city: event.target.value})}
                 />
-                <ColorPicker
-                    name='color'
-                    defaultValue='#000'
-                    // value={this.state.color} - for controlled component
-                    onChange={color => console.log(color)}
-
+                <GithubPicker
+                    triangle='hide'
+                    color={reminderData.color}
+                    onChangeComplete={({hex}) => updateReminder({color: hex})}
                 />
+                <Button
+                    variant='contained' 
+                    color='primary'
+                    onClick={ submitForm }
+                >
+                    Add
+                </Button>
             </form>
             
         </Paper>
